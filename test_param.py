3bea2e66e1ce3deac0263ccd90573a805591d45b
Tekevwe <t.kwakpovwe@gmail.com>
2021-10-30 22:24:15 +0100

    tidying up

--- test_param.py
+++
@@ -1,5 +0,0 @@
-TOP = '/rds/projects/d/divjakd-ooo-machines/Users/tekevwe/English_tense/Data_preparation/'
-WD = TOP + 'Step1_Extract_sentences_with_verbs_BNC'
-BNC_TAGGED_ALL = TOP + 'Data_shared/BNC.tagged.txt'
-RESULTS_TSV = TOP + "Results/sentences_BNC.tsv"
-RESULTS_CSV = TOP + "Results/sentences_BNC.csv"
